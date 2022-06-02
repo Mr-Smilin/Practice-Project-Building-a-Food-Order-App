2047ee5907578d491fe9d57912cb67100ad2e7a8
TW - IT(WEB) - Lion Chuang <lion.chuang@shoalter.com>
2022-06-02 14:20:59 +0800

    修正

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,23 +1,23 @@
 import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
 
-const Backdrop = ({ onClick }) => {
-  return <div className={classes.backdrop} onClick={onClick}></div>
-};
+const Backdrop = ({ onClick }) => (
+  <div className={classes.backdrop} onClick={onClick}></div>
+);
 
-const ModalOverlay = ({ children }) => {
-  return <div className={classes.modal}>
+const ModalOverlay = ({ children }) => (
+  <div className={classes.modal}>
     <div className={classes.content}>{children}</div>
   </div>
-};
+);
 
 const portalElement = document.getElementById('overlays');
 
-const Modal = ({ onClick, children }) => {
-  return <>
+const Modal = ({ onClick, children }) => (
+  <>
     {ReactDOM.createPortal(<Backdrop onClick={onClick} />, portalElement)}
     {ReactDOM.createPortal(<ModalOverlay>{children}</ModalOverlay>, portalElement)}
   </>
-};
+);
 
 export default Modal;
